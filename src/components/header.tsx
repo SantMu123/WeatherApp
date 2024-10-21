844eacb2cf2f0df32adede5badb661a5dc93f024
SantMu123 <smunozcastaneda14@gmail.com>
2024-10-21 15:11:26 -0500

    feat: :lipstick: images, font and tailwind conf addded

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-stone-600 flex">
+      <h3 className="font-bold text-blue-500">Floridablanca, Santander</h3>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/views/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/header.tsx'
+
+const Home: React.FC = () => {
+  return (
+    <Header/>
+  )
+}
+
+export default Home
